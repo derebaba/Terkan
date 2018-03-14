e8d5227667cd35e33e1365c7ff2f6ea66fd4c8f2
derebaba <erdemderebasoglu@hotmail.com>
2018-03-14 08:53:19 +0300

    change mail from address

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@terkan.me'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@mg.terkan.me'),
         'name' => env('MAIL_FROM_NAME', 'Terkan'),
     ],
 
