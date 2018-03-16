4f4b6544bcdc049b39d58a8e69823b35ad304c5d
derebaba <erdemderebasoglu@hotmail.com>
2018-03-16 10:00:54 +0300

    fix trustproxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -11,10 +11,10 @@ class TrustProxies extends Middleware
 	protected $proxies = '*';
 	
 	protected $headers = [
-		Illuminate\Http\Request::HEADER_FORWARDED    => null, // not set on AWS or Heroku
-		Illuminate\Http\Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
-		Illuminate\Http\Request::HEADER_CLIENT_HOST  => null, // not set on AWS or Heroku
-		Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
-		Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
+		Request::HEADER_FORWARDED    => null, // not set on AWS or Heroku
+		Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
+		Request::HEADER_CLIENT_HOST  => null, // not set on AWS or Heroku
+		Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
+		Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
 	];
 }
