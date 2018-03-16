65e1f704295f63a21c46f57b03fa622faa1be407
derebaba <erdemderebasoglu@hotmail.com>
2018-03-16 17:15:46 +0300

    style cookie bar

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,10 +1,10 @@
-<div class="js-cookie-consent cookie-consent offset-3">
+<div class="js-cookie-consent cookie-consent">
 
     <span class="cookie-consent__message">
         {!! trans('cookieConsent::texts.message') !!}
     </span>
 
-    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-sm btn-light">
+    <button id="cookie-agree-button" class="js-cookie-consent-agree cookie-consent__agree btn btn-sm btn-light">
         {{ trans('cookieConsent::texts.agree') }}
     </button>
 
