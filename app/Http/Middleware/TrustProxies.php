bc582b1df9b5db7a2a6ff9ae6907542138ee4aee
derebaba <erdemderebasoglu@hotmail.com>
2018-03-16 09:58:17 +0300

    config trustedproxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,17 +7,14 @@ use Fideloper\Proxy\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * The headers that should be used to detect proxies.
-     *
-     * @var array
-     */
-    protected $proxies = '**';
-
-    /**
-     * The current proxy header mappings.
-     *
-     * @var string
-     */
-	protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
+	
+	protected $proxies = '*';
+	
+	protected $headers = [
+		Illuminate\Http\Request::HEADER_FORWARDED    => null, // not set on AWS or Heroku
+		Illuminate\Http\Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
+		Illuminate\Http\Request::HEADER_CLIENT_HOST  => null, // not set on AWS or Heroku
+		Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
+		Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
+	];
 }
