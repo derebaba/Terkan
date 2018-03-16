806a09d69d1408361afd559733abf5bfb55908fa
derebaba <erdemderebasoglu@hotmail.com>
2018-03-16 10:18:43 +0300

    config trustedproxy 3

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -10,11 +10,5 @@ class TrustProxies extends Middleware
 	
 	protected $proxies = '*';
 	
-	protected $headers = [
-		Request::HEADER_FORWARDED    => null, // not set on AWS or Heroku
-		Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
-		Request::HEADER_CLIENT_HOST  => null, // not set on AWS or Heroku
-		Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
-		Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
-	];
+	protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
