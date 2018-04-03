d7436a7a3a5bda6c8e48cede505df911f319b4a4
derebaba <erdemderebasoglu@hotmail.com>
2018-04-03 09:59:12 +0300

    share watchlist on facebook

--- resources/views/users/watchlist.blade.php
+++ resources/views/users/watchlist.blade.php
@@ -1,11 +1,38 @@
 @extends('layouts.app')
 
+@section('head')
+<!-- You can use Open Graph tags to customize link previews.
+    Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
+  <meta property="og:url"           content="{{ URL::current() }}" />
+  <meta property="og:type"          content="website" />
+  <meta property="og:title"         content="Terkan" />
+  <meta property="og:description"   content="{{ $user->name }}'s watchlist" />
+  <meta property="og:image"         content={{ Cloudder::secureShow($user->pic) }} />
+@endsection
+
 @section('content')
 	@inject('image', 'Tmdb\Helper\ImageHelper')
+
+	<!-- Load Facebook SDK for JavaScript -->
+  <div id="fb-root"></div>
+  <script>
+		(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	</script>
 
 	<div class="card" style="width: 100%;">
 		<div class="card-header">
 			{{ $user->name }}'s watchlist
+
+			<div class="fb-share-button" 
+				data-href="{{ URL::current() }}" 
+				data-layout="button">
+			</div>
 		</div>
 	</div>
 	<div class="row">
