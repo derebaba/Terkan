274a546ef4c8f0777287d1484063936204d52c62
derebaba <erdemderebasoglu@hotmail.com>
2018-04-05 14:11:38 +0300

    log mail variables

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers;
 
 use \App\Jobs\HandleIncomingEmail;
-use App\Models\SendgridParse;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
+use App\Models\SendgridParse;
 
 class MailController extends Controller
 {
     public function pipeEmail(Request $request) {
+		$parsed = new SendgridParse($request);
 		/*$to = $request->input('to');
 		$from = $request->input('from');
 		$html = $request->input('html');
 		$subject = $request->input('subject');*/
+		Log::debug($request);
+		Log::debug($parsed);
 		dispatch(new HandleIncomingEmail($request));
 
 		return response()->json(['success' => 'success'], 200);
