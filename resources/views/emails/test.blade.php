987ce043f2c44942b6565cbef997fe288fde19b4
derebaba <erdemderebasoglu@hotmail.com>
2018-04-05 15:42:33 +0300

    exclude csrf route

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html>
-    <html lang="en-US">
-      <head>
-          <meta charset="utf-8">
-      </head>
-      <body>
-          <h2>Test Email</h2>
-          <p></p>
-      </body>
-    </html>
+@extends('layouts.app')
+
+@section('content')
+	@foreach ($mail as $field)
+		{{ $field }}
+	<br>
+	@endforeach
+@endsection
