95884955f92fcd7d7ed00adda76f6dd23cb8a077
derebaba <erdemderebasoglu@hotmail.com>
2018-04-05 15:18:49 +0300

    only response

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,12 +10,13 @@ use App\Models\SendgridParse;
 class MailController extends Controller
 {
     public function pipeEmail(Request $request) {
-		$mail = new SendgridParse($request);
-		/*$to = $request->input('to');
+		/*$mail = new SendgridParse($request);
+		dd($mail);
+		$to = $request->input('to');
 		$from = $request->input('from');
 		$html = $request->input('html');
-		$subject = $request->input('subject');*/
-		Mail::to('erdemderebaba@gmail.com')->subject('asdsd')->send('cxcxc');
+		$subject = $request->input('subject');
+		Mail::to('erdemderebaba@gmail.com')->subject('asdsd')->send('cxcxc');*/
 		//dispatch(new HandleIncomingEmail($mail));
 
 		return response()->json(['success' => 'success'], 200);
