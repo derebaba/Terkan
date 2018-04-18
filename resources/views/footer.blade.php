8b2d9b4139457b3778340eef03cffed4f26f5a54
derebaba <erdemderebasoglu@hotmail.com>
2018-04-18 21:08:12 +0300

    delete doc ready

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,47 +1,47 @@
 <script>
-$(document).ready(function () {
-	$('#average-rating').barrating({
-		theme: 'fontawesome-stars-o',
-		showSelectedRating: true,
-		initialRating: window.averageRating,
-		readonly: true
-	});
 
-	$('#star-rating').barrating({
-		theme: 'fontawesome-stars-o',
-		showSelectedRating: true,
-		initialRating: 0
-	});
-
-	//	reviews.edit
-	$('#edit-star-rating').barrating({
-		theme: 'fontawesome-stars-o',
-		showSelectedRating: true,
-		initialRating: window.stars,
-	});
-
-	//	search, home:news
-	if ($('#star-rating-0').length) {
-		for (i = 0; i < 40; i++) {
-			$('#star-rating-' + i).barrating({
-				theme: 'fontawesome-stars-o',
-				showSelectedRating: true,
-				initialRating: window.stars[i],
-				readonly: true
-			});
-		}
-	}
-
-	//	home:recommendations
-	if ($('#recommendation-rating-0').length) {
-		for (i = 0; i < 40; i++) {
-			$('#recommendation-rating-' + i).barrating({
-				theme: 'fontawesome-stars-o',
-				showSelectedRating: true,
-				initialRating: window.recommendationStars[i],
-				readonly: true
-			});
-		}
-	}
+$('#average-rating').barrating({
+	theme: 'fontawesome-stars-o',
+	showSelectedRating: true,
+	initialRating: window.averageRating,
+	readonly: true
 });
+
+$('#star-rating').barrating({
+	theme: 'fontawesome-stars-o',
+	showSelectedRating: true,
+	initialRating: 0
+});
+
+//	reviews.edit
+$('#edit-star-rating').barrating({
+	theme: 'fontawesome-stars-o',
+	showSelectedRating: true,
+	initialRating: window.stars,
+});
+
+//	search, home:news
+if ($('#star-rating-0').length) {
+	for (i = 0; i < 40; i++) {
+		$('#star-rating-' + i).barrating({
+			theme: 'fontawesome-stars-o',
+			showSelectedRating: true,
+			initialRating: window.stars[i],
+			readonly: true
+		});
+	}
+}
+
+//	home:recommendations
+if ($('#recommendation-rating-0').length) {
+	for (i = 0; i < 40; i++) {
+		$('#recommendation-rating-' + i).barrating({
+			theme: 'fontawesome-stars-o',
+			showSelectedRating: true,
+			initialRating: window.recommendationStars[i],
+			readonly: true
+		});
+	}
+}
+
 </script>
