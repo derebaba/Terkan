854bd857774752d1701d4453fd7c572e9d0e496f
derebaba <erdemderebasoglu@hotmail.com>
2018-04-20 22:36:53 +0300

    get

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
 		if (!Auth::check())
 			return $this->welcome();
 
-		$reviews = Review::whereIn('user_id', Auth::user()->followings()->get()->pluck('id'))->take(-20)->reverse()->values();
+		$reviews = Review::whereIn('user_id', Auth::user()->followings()->get()->pluck('id'))->take(-20)->get()->reverse()->values();
 		$reviewables = $this->getReviewables($reviews);
 		
 		$movies = Tmdb::getDiscoverApi()->discoverMovies()['results'];
@@ -68,7 +68,7 @@ class HomeController extends Controller
 
 	public function welcome() {
 		//	get latest popular reviews
-		$reviews = Review::all()->reverse()->values();
+		$reviews = Review::all()->take(-20)->get()->reverse()->values();
 		$reviewables = $this->getReviewables($reviews);
 
 		//	get genres for navbar
