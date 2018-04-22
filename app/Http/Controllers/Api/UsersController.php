ffe6cdb466a823112c8af139d0081da5a4f1ff83
derebaba <erdemderebasoglu@hotmail.com>
2018-04-22 20:11:25 +0300

    updateuser request

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers\API;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\UpdateUser;
 
+/**
+ * @resource User
+ *
+ * Get, update users
+ */
 class UsersController extends BaseController
 {
     /**
@@ -21,11 +27,15 @@ class UsersController extends BaseController
     /**
      * Update user.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+	 * @response {
+	 * 	'success': true or false,
+	 * 	'data': all fields of user,
+	 * 	'message': description message (may be empty),
+	 * }
+     * @param  \App\Http\Requests\UpdateUser  $request
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request)
+    public function update(UpdateUser $request)
     {
         $request->user()->update([
 			'name' => $request->input('name')
