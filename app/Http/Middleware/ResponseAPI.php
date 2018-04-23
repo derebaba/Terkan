38f4038d2cb3f511fdd042800bfc516a15abe267
derebaba <erdemderebasoglu@hotmail.com>
2018-04-23 10:40:19 +0300

    set default message format to json

---
+++ app/Http/Middleware/ResponseAPI.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ResponseAPI {
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        $response = $next($request);
+
+        if (in_array($response->status(), [200, 201, 404, 401, 422])) {
+            $response->header('Content-Type', 'application/json');
+        }
+
+        return $response;
+    }
+
+}
