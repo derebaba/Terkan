12ddd98348ae3a759b4fc94aa94a170833ad8725
derebaba <erdemderebasoglu@hotmail.com>
2018-05-30 16:49:01 +0300

    ask for permission to send email during verification

--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -1,2 +1,2 @@
 <h1>Welcome to Terkan</h1>
-Click <a href="{{url('/verifyemail/'.$email_token)}}">here</a> to verify your email
+Click <a href="{{url('/verifyemail/'.$email_token)}}">here</a> to verify your email and allow us to send you emails.
